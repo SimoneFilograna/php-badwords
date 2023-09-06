04727aed412c5bdea97950073ba0bc0dfd3aca3e
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-06 15:21:42 +0200

    add-style

--- filter.php
+++ filter.php
@@ -1,15 +1,9 @@
 <?php
-    var_dump($_POST);
     $parag = $_POST["parag"];
     $old_count = strlen($parag);
     $badword = $_POST["filter"];
     $new_text = str_replace($badword,"***", $parag);
-    $count = strlen($new_text);
-    
-
-
-
-    
+    $count = strlen($new_text);      
 
 ?>
 
@@ -20,16 +14,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parag</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="style-filter.css">
 </head>
 <body>
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <p><?php echo $parag. " ". $old_count ?></p>
-                <p><?php echo $new_text. " ". $count ?></p>
+    <div class="container p-5 mt-3">
+        <h1 class="text-center text-white display-1">FILTERED WORD AND PARAGRAPH</h1>
 
+        <div class="card-container mt-4">
+            <div class="row justify-content-center">
+                <div class="col-4">
+                    <div class="card text-center mt-5 h-100 rounded-pill">
+                        <h3 class="pt-4">Original Text</h3>
+                        <div class="card-body d-flex flex-column">
+                            <h2 class="fs-2 fw-bold"><?php echo $parag ?></h2>   
+                            <p class="mt-auto fs-5"><?php echo "(" . "Length: " . $old_count . ")" ?> </p>           
+                        </div>
+                    </div>                 
+                </div>
+                <div class="col-4">
+                    <div class="card text-center mt-5 h-100 rounded-pill">
+                        <h3 class="pt-4">Filtered Text</h3>
+                        <div class="card-body d-flex flex-column">
+                            <h2 class="fs-2 fw-bold"><?php echo $new_text ?></h2> 
+                            <p class="mt-auto fs-5"><?php echo "(". "Length: " . $count . ")" ?></p>           
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+    
         </div>
+
     </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,24 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad Words</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <h1 class="display-1 text-center">
+    <div class="container p-5 mt-3">
+        <h1 class="display-1 text-center text-white">
             BAD WORDS
         </h1>
         <form action="filter.php" method="POST">  
-            <div class="mb-3">
-                <label for="filter" class="form-label h2">Bad Word</label>
+            <div class="mb-5">
+                <label for="filter" class="form-label h2 text-white">Bad Word</label>
                 <input type="text" name="filter" id="filter" class="form-control">
             </div>
 
-            <div class="mb-3">
-                <label for="parag" class="form-label h2">Your Text</label>
-                <input type="text-area" name="parag" id="parag" class="form-control">
+            <div class="mb-5">
+                <label for="parag" class="form-label h2 text-white">Your Text</label>
+                <input type="text" name="parag" id="parag" class="form-control">
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-outline-light mt-3">Submit</button>
         </form>
     </div>
 </body>
